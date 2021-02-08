dff26f6189393bda5822e64e2cee9c940f379afb
Pedro Ferreira <pedroferreiracjr@gmail.com>
2021-02-08 00:54:18 -0300

    remoção de comentários

--- src/main/java/br/com/dotofcodex/somacarrao_delivery_api/SomacarraoDeliveryApiV2Application.java
+++ src/main/java/br/com/dotofcodex/somacarrao_delivery_api/SomacarraoDeliveryApiV2Application.java
@@ -7,21 +7,8 @@ import org.springframework.cache.annotation.EnableCaching;
 @EnableCaching
 @SpringBootApplication
 public class SomacarraoDeliveryApiV2Application {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(SomacarraoDeliveryApiV2Application.class, args);
 	}
-	
-	/*
-	@Bean
-	public CommandLineRunner run() {
-		return (args) -> {
-			User user = new User();
-			user.setEmail("pedroferreiracjr@gmail.com");
-			user.setPassword(encoder.encode("p3dr0"));
-			user.setEnabled(true);
-			repository.save(user);
-		};
-	}
-	*/
 }
